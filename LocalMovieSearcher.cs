b3e61342c5c1f47b34fa3f69be7bc36e26bf663f
camalzadeelvin <camalzadeelvin@STHQ0127-05>
2022-12-15 10:24:39 +0400

    Add project files.

---
+++ LocalMovieSearcher.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Proxy
+{
+    // Proxy Service
+    public class LocalMovieSearcher : IMovieSearcher
+    {
+        List<Movie> localMovieDB;
+        IMovieSearcher globalMovieSearcher;
+        public LocalMovieSearcher(IMovieSearcher movieSearcher)
+        {
+            globalMovieSearcher = movieSearcher;
+            localMovieDB = new List<Movie>()
+            {
+                new Movie() {Title="A1", Country = "Azerbaijan", Director = "Haci", Genre = "Action", Actors = "Maga"},
+                new Movie() {Title="A2", Country = "Azerbaijan", Director = "Samur", Genre = "Dram", Actors = "Bred Pit"},
+                new Movie() {Title="A3", Country = "Azerbaijan", Director = "Elesger", Genre = "Detective", Actors = "Benedict"}
+
+            };
+        }
+        public Movie SearchByTitle(string title)
+        {
+            if (localMovieDB.Any(x => x.Title == title))
+            {
+                return localMovieDB.FirstOrDefault(x => x.Title == title);
+            }
+            else
+            {
+                return globalMovieSearcher.SearchByTitle(title);
+            }
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Proxy
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                while (true)
+                {
+
+                IMovieSearcher searcher = new LocalMovieSearcher(new GlobalMovieSearcher());   //LocalMovieSearcher -proxy
+                    Console.Write("Serch: ");
+                    ShowMovieData(searcher.SearchByTitle(Console.ReadLine()));
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+            public static void ShowMovieData(Movie movie)
+            {
+                Console.WriteLine($"Country: {movie.Country}\nDirected by: {movie.Director}\n" +
+                    $"Genre: {movie.Genre}\nActors: {movie.Actors}\n\n");
+            }
+    }
+    
+}
